7081eb9d5d2431ec12980f82bb6d2fcb2b7f82c1
Ohad <ohadfefer16@gmail.com>
2026-03-16 11:39:03 +0200

    matched background color of price graph

--- frontend/src/components/details/price-chart.tsx
+++ frontend/src/components/details/price-chart.tsx
@@ -74,8 +74,8 @@ export function PriceChart({ symbol }: PriceChartProps) {
         hide_volume: true,
         save_image: false,
         calendar: false,
-        backgroundColor: "rgba(0, 0, 0, 0)",
-        gridColor: "rgba(255, 255, 255, 0.06)",
+        backgroundColor: "#0a0b0d",
+        gridColor: "#0a0b0d",
         container_id: containerId,
       })
     })
